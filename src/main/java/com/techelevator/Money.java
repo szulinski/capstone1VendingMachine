00dde34b0e0203429214f2828145c8d8da0fcce2
Susanne Clark <susanneaclark@gmail.com>
2022-02-10 12:08:55 -0500

    Changes to money

--- src/main/java/com/techelevator/Money.java
+++ src/main/java/com/techelevator/Money.java
@@ -1,14 +1,24 @@
 package com.techelevator;
 
+import java.math.BigDecimal;
+
 public class Money {
+
+    private BigDecimal availableFunds;
 
     public Money()
     {
-
+        availableFunds = new BigDecimal(10.00);
     }
 
+    public void addFunds(BigDecimal newFunds)
+    {
+        availableFunds.add(newFunds) ;
+
+    }
     public void purchaseItem()
     {
+
 
     }
 }
