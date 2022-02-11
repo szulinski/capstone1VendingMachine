0f98d0e01b108dd5469bbebb5dfba418c3d64372
Susanne Clark <susanneaclark@gmail.com>
2022-02-11 15:59:01 -0500

    Logic

--- src/main/java/com/techelevator/VendingMachine.java
+++ src/main/java/com/techelevator/VendingMachine.java
@@ -2,12 +2,14 @@ package com.techelevator;
 
 import java.io.File;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class VendingMachine {
 
     private String inputFileString = "vendingmachine.csv";
     private Inventory inventory;
     private Money money;
+    private Audit audit;
 
     //public VendingMachine(File inputFile)
     public VendingMachine()
@@ -15,10 +17,29 @@ public class VendingMachine {
         //this.inputFileString = ;
         loadInventory();
         money = new Money();
+
+        audit = new Audit();
+        BigDecimal addFunds = new BigDecimal(1.50);
+        addFunds.setScale(2);
+        addFunds(addFunds);
+        cashOut();
+        closeApplication();
+        //audit.createAuditEntry("Purchase Made");
     }
 
     private void cashOut() {
+
+        BigDecimal startingBalance = money.getAvailableFunds();
+        String startingBalanceString = "$" + startingBalance.toString() + " ";
+
         money.cashOut();
+
+        BigDecimal endingBalance = money.getAvailableFunds();
+        String endingBalanceString = "$" + endingBalance.toString() + " ";
+
+        String message = "GIVE CHANGE: " + startingBalanceString + endingBalanceString;
+
+        audit.createAuditEntry(message);
     }
 
     private void loadInventory()
@@ -50,12 +71,32 @@ public class VendingMachine {
         return isStocked;
     }
 
-        public void addFunds(BigDecimal funds)
+    public void addFunds(BigDecimal funds)
     {
         try {
+            funds.setScale(2);
+            String startingBalanceString = "$" + funds.toString() + " ";
+           //BigDecimal startingBalance = money.getAvailableFunds();
+            //String startingBalanceString = "$" + startingBalance.toString() + " ";
+
             money.addFunds(funds);
+
+            BigDecimal endingBalance = money.getAvailableFunds();
+            String endingBalanceString = "$" + endingBalance.toString() + " ";
+
+            String message = "FEED MONEY:  " + startingBalanceString + endingBalanceString;
+
+
+            audit.createAuditEntry(message);
+
+
         } catch (ArithmeticException e) {
             e.printStackTrace();
         }
+    }
+
+    private void closeApplication()
+    {
+        audit.closeAudit();
     }
 }
