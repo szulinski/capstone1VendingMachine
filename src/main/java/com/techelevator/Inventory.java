196a658dc7880edb41d385b021948e6316fd8c74
Susanne Clark <susanneaclark@gmail.com>
2022-02-11 11:21:48 -0500

    Working again

--- src/main/java/com/techelevator/Inventory.java
+++ src/main/java/com/techelevator/Inventory.java
@@ -17,12 +17,7 @@ public class Inventory {
     }
 
     private void loadInitialItemsFile() {
-        try {
             InitialItemsFile initialItemsFile = new InitialItemsFile(inputFile, this);
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-//            Feels like this should take 404 catch but won't accept it
-        }
     }
 
     public void addToInventory(Item item)
@@ -43,16 +38,15 @@ public class Inventory {
 //    Demonstration TryCatch. Line 56 now has a "missing return issue." Put it outside trycatch, isStocked is red.
     public boolean removeFromInventory(String slotLocation)
     {
-        try {
+
             Item item = itemList.get(slotLocation);
             boolean isStocked = item.removeFromInventory();
             // if stocked is false; return message to user interface that item not in stock
             // send message to the audit trail
             // handle money
             return isStocked;
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-        }
+
+
     }
 
     public String getProductName(String slotLocation) {
@@ -82,7 +76,8 @@ public class Inventory {
 
 
 //    We need to have a search method to find slot location
-//    public String getSlotLocations()
+//    public String getSlotLocation()
+//  {
 //    functionality to display slot locations to UI
 //    }
-}
+
