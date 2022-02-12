29dee20e3bdf2cb9a6b9710030157a3e6156ec41
Susanne Clark <susanneaclark@gmail.com>
2022-02-12 09:38:09 -0500

    List Inventory

--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -9,27 +9,42 @@ public class VendingMachineCLI {
 	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE };
 
 	private VendingMachineUI menu;
+	private static VendingMachine vendingMachine;
 
 	public VendingMachineCLI(VendingMachineUI menu) {
 		this.menu = menu;
 	}
 
 	public void run() {
-//		while (true) {
-//			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
-//
-//			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
-//				// display vending machine items
-//			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
-//				// do purchase
-//			}
-		//}
+
+		try {
+			while (true) {
+				String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
+
+				if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
+					String[] menuChoices = vendingMachine.createInventoryArray();
+					int menuChoicesLength = menuChoices.length;
+
+					for (int i = 0; i < menuChoicesLength; i++) {
+						System.out.println(menuChoices[i]);
+					}
+
+				} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
+					//1choice.
+				}
+			}
+		}
+		catch (Exception e)
+		{
+			System.out.println ("An exception occured.");
+
+		}
 	}
 	public static void main(String[] args) {
 
-		VendingMachine vendingMachine = new VendingMachine();
-//		VendingMachineUI menu = new VendingMachineUI(System.in, System.out);
-//		VendingMachineCLI cli = new VendingMachineCLI(menu);
-//		cli.run();
+		vendingMachine = new VendingMachine();
+		VendingMachineUI menu = new VendingMachineUI(System.in, System.out);
+		VendingMachineCLI cli = new VendingMachineCLI(menu);
+		cli.run();
 	}
 }
