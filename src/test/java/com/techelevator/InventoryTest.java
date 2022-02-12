40a27c505c132e1b85b9204cc50a678e739164ee
Susanne Clark <susanneaclark@gmail.com>
2022-02-12 11:40:04 -0500

    Updated test

--- src/test/java/com/techelevator/InventoryTest.java
+++ src/test/java/com/techelevator/InventoryTest.java
@@ -1,4 +1,21 @@
 package com.techelevator;
 
+import org.junit.Test;
+
+import java.io.File;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static org.junit.Assert.assertTrue;
+
 public class InventoryTest {
+
+    File testFile = new File("testfile.txt");
+    Inventory testInventory = new Inventory(testFile);
+    InitialItemsFile testingItemsFile = new InitialItemsFile(testFile, testInventory);
+
+    @Test
+    public void INVENTORY_CHECK_TEST() {
+        assertTrue(testInventory.isInInventory("testing"));
+    }
 }
