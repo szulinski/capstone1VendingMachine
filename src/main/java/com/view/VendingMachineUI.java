f9f13e644edcb3cfacc80a2dd8b31966a6aad153
Susanne Clark <susanneaclark@gmail.com>
2022-02-14 06:44:50 -0500

    Working again

---
+++ src/main/java/com/view/VendingMachineUI.java
@@ -0,0 +1,56 @@
+package com.techelevator.view;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class VendingMachineUI {
+
+    private PrintWriter out;
+    private Scanner in;
+
+    public VendingMachineUI(InputStream input, OutputStream output) {
+        this.out = new PrintWriter(output);
+        this.in = new Scanner(input);
+    }
+
+    public Object getChoiceFromOptions(Object[] options) {
+        Object choice = null;
+        while (choice == null) {
+            displayMenuOptions(options);
+            choice = getChoiceFromUserInput(options);
+        }
+        return choice;
+    }
+
+    private Object getChoiceFromUserInput(Object[] options) {
+        Object choice = null;
+        String userInput = in.nextLine();
+        try {
+            int selectedOption = Integer.valueOf(userInput);
+            if (selectedOption > 0 && selectedOption <= options.length) {
+                choice = options[selectedOption - 1];
+            }
+        } catch (NumberFormatException e) {
+            // eat the exception, an error message will be displayed below since choice will be null
+        }
+        if (choice == null) {
+            out.println(System.lineSeparator() + "*** " + userInput + " is not a valid option ***" + System.lineSeparator());
+        }
+        return choice;
+    }
+
+    private void displayMenuOptions(Object[] options) {
+        out.println();
+        DecimalFormat formatter = new DecimalFormat("0");
+        for (int i = 0; i < options.length; i++) {
+            int optionNum = i + 1;
+            String optFormatted = formatter.format(optionNum);
+            out.println(optFormatted + ") " + options[i]);
+        }
+        out.print(System.lineSeparator() + "Please choose an option >>> ");
+        out.flush();
+    }
+}
