3ed8e5a45eed3e5fb72ef99fc5a0126e54314349
Susanne Clark <susanneaclark@gmail.com>
2022-02-14 06:54:00 -0500

    Working again

--- src/test/java/com/techelevator/VendingMachineTest.java
+++ src/test/java/com/techelevator/VendingMachineTest.java
@@ -3,13 +3,54 @@ package com.techelevator;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import com.techelevator.VendingMachine;
 
 public class VendingMachineTest {
 
     @Test
+    public void constructor_Test(){
+
+        //Arrange
+        // delete AuditLogTest.txt
+        String inventoryString = "VendingMachineTest.csv";
+        String auditLog = "AuditLogTest.txt";
+        String salesReport = "SalesReport.csv";
+
+        // Act
+        com.techelevator.VendingMachine vendingMachine = new VendingMachine(inventoryString, auditLog, salesReport);
+
+        File newAuditTest = new File (auditLog);
+
+        // Assert
+        Assert.assertEquals(true, newAuditTest.exists());
+    }
+
+    @Test
     public void cash_out_test_normal_data() {
+
+        //Arrange
+        String inventoryString = "VendingMachineTest.csv";
+        String auditLog = "AuditLogTest.txt";
+        String salesReport = "SalesReport.csv";
+        VendingMachine vendingMachine = new VendingMachine(inventoryString, auditLog, salesReport);
+
+        BigDecimal addedFunds = new BigDecimal("6.90");
+        vendingMachine.addFunds(addedFunds);
+        addedFunds = addedFunds.setScale(2);
+        String expectedChange = "\n27 Quarters 1 Dimes 1 Nickels";
+
+        // Act
+        String change = vendingMachine.cashOut();
+
+        // Assert
+        Assert.assertEquals(expectedChange, change);
+    }
+
+    @Test
+    public void Cash_out_test_normal_data() {
 
         //Arrange
         String inventoryString = "VendingMachineTest.csv";
