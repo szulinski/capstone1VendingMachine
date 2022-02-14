5a7c9dcfce24b65d59a92a74d389c9ee04320c68
Susanne Clark <susanneaclark@gmail.com>
2022-02-14 02:22:42 -0500

    Additional testing

--- src/test/java/com/techelevator/VendingMachineTest.java
+++ src/test/java/com/techelevator/VendingMachineTest.java
@@ -7,16 +7,61 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class VendingMachineTest {
-/*
+
     @Test
-    public void cash_out_test() {
-        VendingMachine vendingMachine = new VendingMachine();
-        Money money = new Money();
-        vendingMachine.cashOut();
-        BigDecimal availableFunds = money.getAvailableFunds();
-        BigDecimal expectedFunds = new BigDecimal(0.00);
-        Assert.assertEquals(expectedFunds.setScale(2, RoundingMode.DOWN), availableFunds);
+    public void cash_out_test_normal_data() {
+
+        //Arrange
+        String inventoryString = "VendingMachineTest.csv";
+        String auditLog = "AuditLogTest.txt";
+        String salesReport = "SalesReport.csv";
+        VendingMachine vendingMachine = new VendingMachine(inventoryString, auditLog, salesReport);
+
+        BigDecimal addedFunds = new BigDecimal("6.90");
+        vendingMachine.addFunds(addedFunds);
+        addedFunds = addedFunds.setScale(2);
+        String expectedChange = "\n27 Quarters 1 Dimes 1 Nickels";
+
+        // Act
+        String change = vendingMachine.cashOut();
+
+        // Assert
+        Assert.assertEquals(expectedChange, change);
     }
-    */
+    @Test
+    public void cash_out_test_zero() {
+
+        //Arrange
+        String inventoryString = "VendingMachineTest.csv";
+        String auditLog = "AuditLogTest.txt";
+        String salesReport = "SalesReport.csv";
+        VendingMachine vendingMachine = new VendingMachine(inventoryString, auditLog, salesReport);
+
+        String expectedChange = "\n0 Quarters 0 Dimes 0 Nickels";
+
+        // Act
+        String change = vendingMachine.cashOut();
+
+        // Assert
+        Assert.assertEquals(expectedChange, change);
+    }
+
+/*
+    public Open_Add_C()
+    {
+        String changeString = "";
+
+        BigDecimal availableFunds = money.getAvailableFunds();
+        BigDecimal zeroBalance = new BigDecimal(0);
+
+
+        if (availableFunds.compareTo(zeroBalance) > 0)
+        {
+            changeString = cashOut();
+        }
+        audit.closeAudit();
+        return changeString;
+    }*/
+
 
 }
