f30b3ee25f38770958e2d3d1668fda6f635465b9
Susanne Clark <susanneaclark@gmail.com>
2022-02-14 00:34:29 -0500

    Redoing testing

--- src/test/java/com/techelevator/InitialItemsFileTest.java
+++ src/test/java/com/techelevator/InitialItemsFileTest.java
@@ -14,8 +14,58 @@ import static org.junit.Assert.assertTrue;
 
 public class InitialItemsFileTest {
 
+   @Test
+    public void Initial_Items_File_Normal_Data()
+    {
+        // Arrange
+        String InitialItemsString  = "VendingMachineTest.csv";
+        int expectedListLength = 4;
+
+        // Act
+        Inventory inventoryTest = new Inventory(InitialItemsString);
+        int listLength = inventoryTest.getInventoryListLength();
+
+        // Test
+        Assert.assertEquals(expectedListLength, listLength);
+
+    }
+
     @Test
-    public void INITIALIZATION_TEST_NULL_FILE() {
+    public void Initial_Items_File_No_Data()
+    {
+        // Arrange
+        String InitialItemsString  = "";
+        int expectedListLength = 0;
+
+        // Act
+        Inventory inventoryTest = new Inventory(InitialItemsString);
+        int listLength = inventoryTest.getInventoryListLength();
+
+        // Test
+        Assert.assertEquals(expectedListLength, listLength);
+    }
+
+
+   /* @Test
+    public void initial_Items_File_Null_File_Name()
+    {
+        // Arrange
+        String InitialItemsString  = null;
+
+        // Act
+        Inventory inventoryTest = new Inventory(InitialItemsString);
+        //int listLength = inventoryTest.getInventoryListLength();
+
+
+        // Test
+        Assert.assertEquals(expectedListLength, listLength);
+
+    }*/
+
+
+
+
+    /*public void INITIALIZATION_TEST_NULL_FILE() {
         File inputFile;
         File inventoryFile = new File("testing.txt");
         Map<String, Item> itemList = new TreeMap<>();
@@ -32,5 +82,5 @@ public class InitialItemsFileTest {
         InitialItemsFile testingItemsFile = new InitialItemsFile(testFile, testInventory);
         //assert(testInventory.itemList.containsKey("testing"));
 
-    }
+    }*/
 }
