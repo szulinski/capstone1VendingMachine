bfb2dfe2ddfc38b6a370e0e5aeb4731ae7fd236d
Susanne Clark <susanneaclark@gmail.com>
2022-02-14 00:35:25 -0500

    Redoing testing

--- src/test/java/com/techelevator/VendingMachineTest.java
+++ src/test/java/com/techelevator/VendingMachineTest.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class VendingMachineTest {
-
+/*
     @Test
     public void cash_out_test() {
         VendingMachine vendingMachine = new VendingMachine();
@@ -17,4 +17,6 @@ public class VendingMachineTest {
         BigDecimal expectedFunds = new BigDecimal(0.00);
         Assert.assertEquals(expectedFunds.setScale(2, RoundingMode.DOWN), availableFunds);
     }
+    */
+
 }
