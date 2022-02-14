678606dfa3fc09466ac6933a6049c14728883cc6
Susanne Clark <susanneaclark@gmail.com>
2022-02-14 11:50:03 -0500

    Working

--- src/test/java/com/techelevator/SalesReportTest.java
+++ src/test/java/com/techelevator/SalesReportTest.java
@@ -1,12 +1,24 @@
-//package com.techelevator;
-//
-//import org.junit.Test;
-//
-//public class SalesReportTest {
-//
-//    @Test
-//    public void sales_Report_Test()
-//    {
-//
-//    }
-//}
+package com.techelevator;
+
+import com.techelevator.SalesReport;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SalesReportTest {
+
+    @Test
+    public void Sales_Report_Test()
+    {
+        // Arrange
+        String salesReportFileName = "SalesReportTest.csv";
+        SalesReport salesReport = new SalesReport(salesReportFileName);
+        String expectedString = "class com.techelevator.SalesReport";
+
+        // Act
+        String salesReportString = salesReport.getClass().toString();
+
+        // Assert
+        Assert.assertEquals(expectedString, salesReportString);
+
+    }
+}
