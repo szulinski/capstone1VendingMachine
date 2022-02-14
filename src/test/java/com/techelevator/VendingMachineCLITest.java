923ead816705ddbc8ab6001209fc86a5285bd989
Susanne Clark <susanneaclark@gmail.com>
2022-02-14 11:50:55 -0500

    Working

--- src/test/java/com/techelevator/VendingMachineCLITest.java
+++ src/test/java/com/techelevator/VendingMachineCLITest.java
@@ -1,4 +1,24 @@
 package com.techelevator;
 
+import com.techelevator.VendingMachineCLI;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class VendingMachineCLITest {
+
+    /*@Test
+    public void VendingMachineCLI_Constructor_Test()
+    {
+        // Arrange
+        VendingMachineCLI runner = new VendingMachineCLI();
+        String expectedString = "class com.techelevator.VendingMachineCLI";
+
+        // Act
+        String runnerString = runner.getClass().toString();
+
+        // Assert
+        Assert.assertEquals(expectedString, runnerString);
+
+    }*/
+
 }
