c8abc3bc0e62abb62f9f77dee70e06cb152d909b
Susanne Clark <susanneaclark@gmail.com>
2022-02-14 06:53:28 -0500

    Working again

--- src/test/java/com/techelevator/SalesReportTest.java
+++ src/test/java/com/techelevator/SalesReportTest.java
@@ -1,12 +1,12 @@
-package com.techelevator;
-
-import org.junit.Test;
-
-public class SalesReportTest {
-
-    @Test
-    public void sales_Report_Test()
-    {
-
-    }
-}
+//package com.techelevator;
+//
+//import org.junit.Test;
+//
+//public class SalesReportTest {
+//
+//    @Test
+//    public void sales_Report_Test()
+//    {
+//
+//    }
+//}
