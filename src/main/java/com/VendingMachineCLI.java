268f5a56bf6651cac479d53c5dce3e49a14a71d3
Susanne Clark <susanneaclark@gmail.com>
2022-02-14 11:53:16 -0500

    Working

--- src/main/java/com/VendingMachineCLI.java
+++ src/main/java/com/VendingMachineCLI.java
@@ -14,7 +14,7 @@ public class VendingMachineCLI {
     private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
     private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
     private static final String MAIN_MENU_OPTION_EXIT = "Exit";
-    private static final String MAIN_MENU_OPTION_SALES_REPORT = "Sales Report";
+    private static final String MAIN_MENU_OPTION_SALES_REPORT = "";//"Sales Report";
 
     private static final String[] MAIN_MENU_OPTIONS = {MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE,
             MAIN_MENU_OPTION_EXIT, MAIN_MENU_OPTION_SALES_REPORT};
